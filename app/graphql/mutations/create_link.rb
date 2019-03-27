1c27197152ac10b3d79344ba3723fe1ffbbfce1d
Alireza Andersen <alireza.andersen@gmail.com>
2019-03-27 14:41:54 -0600

    created mutations

--- app/graphql/mutations/create_link.rb
+++ app/graphql/mutations/create_link.rb
@@ -1,5 +1,17 @@
 module Mutations
-  class CreateLInk < BaseMutation
+  class CreateLink < BaseMutation
+    # arguments passed to the `resolved` method
+    argument :description, String, required: true
+    argument :url, String, required: true
 
+    # return type from the mutation
+    type Types::LinkType
+
+    def resolve(description: nil, url: nil)
+      Link.create!(
+        description: description,
+        url: url,
+      )
+    end
   end
-end 
+end
