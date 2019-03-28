b7e96c433d0ca2c63e782642a075671fbd38b4b7
Alireza Andersen <alireza.andersen@gmail.com>
2019-03-28 11:46:36 -0600

    introduced votes to graphql

--- app/graphql/types/link_type.rb
+++ app/graphql/types/link_type.rb
@@ -6,6 +6,7 @@ module Types
     # `posted_by` is automatically camelcased as `postedBy`
     # field can be nil, because we added users relationship later
     # "method" option remaps field to an attribute of Link model
-    field :posted_by, UserType, null: true, method: :user
+    field :posted_by, UserType, null: false, method: :user
+    field :votes, [Types::VoteType], null: false
   end
 end
