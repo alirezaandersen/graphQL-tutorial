45a7f84e2804acecf1c1b51b77264c7c698f4149
Alireza Andersen <alireza.andersen@gmail.com>
2019-03-28 14:22:07 -0600

    created associations

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,9 @@
-class User < ApplicationRecord
+class User < ActiveRecord::Base
   has_secure_password
 
-  validates :name, presence: true
+  has_many :links, dependent: :destroy
+  has_many :votes, dependent: :destroy
+
+  validates :name, presence: true, length: { minimum: 3 }
   validates :email, presence: true, uniqueness: true
 end
